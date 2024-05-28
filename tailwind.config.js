700d22b13cba96c965da06157df8a606de546a19
Aditya Rao <dev.adityarao@gmail.com>
2024-05-28 19:23:10 +0530

    Complete styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      main: "hsl(217, 54%, 11%)",
+      card: "hsl(216, 50%, 16%)",
+      line: "hsl(215, 32%, 27%)",
+      white: "hsl(0, 0%, 100%)",
+      cyan: "hsl(178, 100%, 50%)",
+      cyanLight: "hsla(178, 100%, 50%, 0.5)",
+      softBlue: "hsl(215, 51%, 70%)",
+    },
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"],
+    },
   },
   plugins: [],
 };
